a5053e9e3bb456f0ab3cef871cf838a34938d82b
kaina404 <kaina404>
2019-11-20 21:22:01 +0800

    init

--- lib/page/player/VideoPlayerPage.dart
+++ lib/page/player/VideoPlayerPage.dart
@@ -1,25 +1,68 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_video_player/flutter_simple_video_player.dart';
+import 'package:quafulplayer/widget/loading_widget.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoPlayerPage extends StatelessWidget {
+class VideoPlayerPage extends StatefulWidget {
   final String url;
 
   const VideoPlayerPage({Key key, this.url}) : super(key: key);
 
   @override
+  _VideoPlayerPageState createState() => _VideoPlayerPageState();
+}
+
+class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  VideoPlayerController _controller;
+  bool loadingFinish = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.url)
+      ..initialize().then((_) {
+        loadingFinish = true;
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (!loadingFinish) {
+      return Scaffold(
+        body: LoadingWidget.getLoading(),
+      );
+    }
     return Scaffold(
-      body: SimpleViewPlayer(
-        url,
-        isFullScreen: false,
+      body: Center(
+        child: _controller.value.initialized
+            ? AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller),
+        )
+            : Container(),
       ),
-      floatingActionButton: IconButton(
-        icon: Icon(Icons.arrow_back),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pop(context);
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
         },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
